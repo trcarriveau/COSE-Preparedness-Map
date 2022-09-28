8a3834de0f60c5a6205a0f459b6173edf5c3e852
j-knudson <71792574+j-knudson@users.noreply.github.com>
2022-09-28 13:16:19 -0500

    Year component updated to now take background color as a parameter; Year output in map updated with color codes matching the wireframe

--- front-end/pages/map.js
+++ front-end/pages/map.js
@@ -4,26 +4,28 @@ import Year from "../Components/Year";
 function Map() {
   return (
     <div className="App">
-        <Year
-           color= '#f2f2f2'
-           yearNumber='1' 
-           studentClass='Freshman'
-        />
-        <Year
-           color= '#f2f2f2'
-           yearNumber='2' 
-           studentClass='Sophomore'
-        />
-        <Year
-           color= '#f2f2f2'
-           yearNumber='3' 
-           studentClass='Junior'
-        />
-        <Year
-           color= '#f2f2f2'
-           yearNumber='4' 
-           studentClass='Senior'
-        />
+        <div style={{backgroundColor: 'white', display: "flex", justifyContent: "space-around", margin: '10px'}}>
+            <Year
+            color= '#f2f2f2'
+            yearNumber='1' 
+            studentClass='Freshman'
+            />
+            <Year
+            color= '#d9d9d9'
+            yearNumber='2' 
+            studentClass='Sophomore'
+            />
+            <Year
+            color= '#bfbfbf'
+            yearNumber='3' 
+            studentClass='Junior'
+            />
+            <Year
+            color= '#a6a6a6'
+            yearNumber='4' 
+            studentClass='Senior'
+            />
+        </div>
         {/* <p>Hello 2</p>
         <div class = "flex-container"> 
           <div class="item"> Item 1</div>
