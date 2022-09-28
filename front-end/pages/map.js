37c249bf65ae2f330483efe378a7c8384a0dcb51
j-knudson <71792574+j-knudson@users.noreply.github.com>
2022-09-28 12:40:33 -0500

    Created a year component and added a set of four years to the base view map

--- front-end/pages/map.js
+++ front-end/pages/map.js
@@ -1,15 +1,36 @@
-//TODO  screen for Career Mapping
+import Year from "../Components/Year";
+
 
 function Map() {
   return (
     <div className="App">
-        <p>Hello</p>
+        <Year
+           color= '#f2f2f2'
+           yearNumber='1' 
+           studentClass='Freshman'
+        />
+        <Year
+           color= '#f2f2f2'
+           yearNumber='2' 
+           studentClass='Sophomore'
+        />
+        <Year
+           color= '#f2f2f2'
+           yearNumber='3' 
+           studentClass='Junior'
+        />
+        <Year
+           color= '#f2f2f2'
+           yearNumber='4' 
+           studentClass='Senior'
+        />
+        {/* <p>Hello 2</p>
         <div class = "flex-container"> 
           <div class="item"> Item 1</div>
           <div class="item"> Item 2</div>
           <div class="item"> Item 3</div>
           <div class="year" style={{backgroundColor: 'grey'}}> Year 4 <p> Senior</p></div>
-        </div>      
+        </div>       */}
     </div>
   );
 }
