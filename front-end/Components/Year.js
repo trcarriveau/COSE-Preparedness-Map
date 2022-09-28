41637b39be2f48a5752f9264428718836c36d135
j-knudson <71792574+j-knudson@users.noreply.github.com>
2022-09-28 13:16:19 -0500

    Year component updated to now take background color as a parameter; Year output in map updated with color codes matching the wireframe

--- front-end/Components/Year.js
+++ front-end/Components/Year.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Year = ({color, yearNumber, studentClass}) => {
   return (
-    <>
-      {/* style={{backgroundColor: color}} */}
+    <div style={{ backgroundColor: color }}>
+      
       <p>Year {yearNumber} </p>
       <p> {studentClass} </p>
-    </>
+    </div>
   )
 }
 
