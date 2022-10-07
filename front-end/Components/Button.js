5470314a2019b666d63c2b6522cf77ddfc4174ae
j-knudson <71792574+j-knudson@users.noreply.github.com>
2022-10-06 19:06:07 -0500

    Matched index.js to wireframe and created reusable button component

---
+++ front-end/Components/Button.js
@@ -0,0 +1,49 @@
+import Colors from "./Colors"
+import PropTypes from 'prop-types'
+import React from "react"
+
+const Button = React.forwardRef(({color, text, onClick, href }, ref) => {
+// const Button = ({ color, text, onClick }) => {
+  return (
+    <>
+    {/* <a href={href} ref={ref} > */}
+            <button style={{
+                backgroundColor: color,
+                width: '100%',
+                padding: '1rem 2rem',
+                // fontWeight: 'bold',
+                fontSize: '1.1rem',
+                color: 'white',
+                borderStyle: 'solid',
+                borderColor: 'black', 
+                borderRadius: '10px',
+                borderWidth: 'thin',
+                outline: 'none',
+                cursor: 'pointer',
+                textAlign: 'center',
+                
+                }} 
+                onClick={onClick} 
+                href={href}
+                ref={ref}
+            >
+                
+                {text}                   
+                
+            </button>
+        {/* </a>  */}
+    </>
+  )
+})
+
+Button.defaultProps = {
+    color: Colors.button
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func,
+}
+
+export default Button
--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -2,26 +2,62 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link';
+import Colors from '../Components/Colors'
+import Button from '../Components/Button';
+
 
 export default function Home() {
+  const onClick = () => {
+    console.log("clicked")
+  }
+
+
   return (
     <div className={styles.body}>
+      <Image
+        src="/../public/huskies_logo.png"
+        layout="fill"
+        objectFit='contain'
+      />
       <div className={styles.container}>
         <form id="login">
-              <h1 className={styles.title}>Login</h1>
+          <div>
+            <h1 className={styles.title}>Login</h1>
+            <hr style={{ color: 'white'}} />
+          </div>
+          <p className={styles.textInputTitle}>
+              Username or Email:
+          </p>
               <div className={styles.group}>
                   <input type="text" className={styles.input1} autoFocus placeholder="Username or email" />
               </div>
+          <p className={styles.textInputTitle}>
+              Password:
+          </p>    
               <div className={styles.group}>
                   <input type="password" className={styles.input1} autoFocus placeholder="Password" />
               </div>
-              <button className={styles.button} type="submit">Sign In</button>
-              <p></p>
-              <p className={styles.next}>
-                  <a className={styles.link} href="/registration" id="linkLogin">Don't have an account? Create account</a>
-              </p>
+              <button className={styles.button3} style={{backgroundColor: Colors.button}} type="submit">Sign In</button>
           </form>
+          <p className={styles.text}>
+              Forgot Password? 
+          </p>
+          <Button
+            color={Colors.button}
+            text = {'Reset Password'}
+            onClick = {onClick}
+           />
+          <p className={styles.text}>
+              Don't have an account?
+          </p>
+          <Link href="/registration" passHref >
+            <Button
+              color={Colors.button}
+              text = {'Register'}
+            />
+          </Link> 
       </div>
+
     </div>
   )
 }
