807a3e9ca619b0919d85dc224cd8f0b128d86403
j-knudson <71792574+j-knudson@users.noreply.github.com>
2022-10-07 17:44:40 -0500

    Formatted index.js and registration.js

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -1,106 +1,101 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link';
-import { useState } from 'react';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import { useState } from "react";
 
 //our components
-import Colors from '../Components/Colors'
-import Button from '../Components/Button';
+import Colors from "../Components/Colors";
+import Button from "../Components/Button";
 
 //icons
-import {AiFillEye} from 'react-icons/ai'
-import {AiFillEyeInvisible} from 'react-icons/ai'
+import { AiFillEye } from "react-icons/ai";
+import { AiFillEyeInvisible } from "react-icons/ai";
 
 export default function Home() {
-  
   //example function that can be passed in a button component
   //example on line 74
   const onClickExample = () => {
-    console.log("clicked")
-  }
+    console.log("clicked");
+  };
 
-  const [passwordShown, setPasswordShown] = useState(false); 
+  const [passwordShown, setPasswordShown] = useState(false);
 
   const togglePassword = () => {
     setPasswordShown(!passwordShown);
-  }
-   
- 
+  };
 
   return (
     <div className={styles.body}>
       <Image
         src="/../public/huskies_logo.png"
         layout="fill"
-        objectFit='contain'
+        objectFit="contain"
       />
       <div className={styles.container}>
         <div>
           <h1 className={styles.title}>Login</h1>
-          <hr style={{ color: 'white'}} />
+          <hr style={{ color: "white" }} />
         </div>
         <form id="login">
-          <p className={styles.textInputTitle}>
-              Username or Email:
-          </p>
+          <p className={styles.textInputTitle}>Username or Email:</p>
           <div className={styles.group}>
-              <input 
-                type="text" 
-                className={styles.input1} 
-                autoFocus 
-                placeholder="Username or email" 
-                // id =""
-                // name = ""
-                required
-              />
-              
+            <input
+              type="text"
+              className={styles.input1}
+              autoFocus
+              placeholder="Username or email"
+              // id =""
+              // name = ""
+              required
+            />
           </div>
-          <p className={styles.textInputTitle}>
-              Password:
-          </p>    
+          <p className={styles.textInputTitle}>Password:</p>
           <div className={styles.group}>
-              <input 
-                type= {passwordShown ? "text" : "password"}
-                className={styles.input1} 
-                placeholder="Minimum length 8 characters" 
-                // id =""
-                // name = "" 
-                required
-                minLength={8}   
+            <input
+              type={passwordShown ? "text" : "password"}
+              className={styles.input1}
+              placeholder="Minimum length 8 characters"
+              // id =""
+              // name = ""
+              required
+              minLength={8}
+            />
+            {passwordShown ? (
+              <AiFillEye
+                className={styles.inputIcon}
+                size="35px"
+                color={Colors.button}
+                onClick={togglePassword}
               />
-            {passwordShown ? 
-                <AiFillEye className={styles.inputIcon} size='35px'color={Colors.button} onClick={togglePassword}/> : 
-                <AiFillEyeInvisible className={styles.inputIcon} size='35px' color={Colors.button} onClick={togglePassword}/>
-            }  
+            ) : (
+              <AiFillEyeInvisible
+                className={styles.inputIcon}
+                size="35px"
+                color={Colors.button}
+                onClick={togglePassword}
+              />
+            )}
           </div>
-          <button 
-            className={styles.button3} 
-            style={{backgroundColor: Colors.button}} 
+          <button
+            className={styles.button3}
+            style={{ backgroundColor: Colors.button }}
             type="submit"
-          >  
+          >
             Sign In
           </button>
         </form>
-        <p className={styles.text}>
-            Forgot Password? 
-        </p>
+        <p className={styles.text}>Forgot Password?</p>
         <Button
           color={Colors.button}
-          text = {'Reset Password'}
-          onClick = {onClickExample}
-          />
-        <p className={styles.text}>
-            Don't have an account?
-        </p>
-        <Link href="/registration" passHref >
-          <Button
-            color={Colors.button}
-            text = {'Register'}
-          />
-        </Link> 
+          text={"Reset Password"}
+          onClick={onClickExample}
+        />
+        <p className={styles.text}>Don't have an account?</p>
+        <Link href="/registration" passHref>
+          <Button color={Colors.button} text={"Register"} />
+        </Link>
       </div>
-      
     </div>
-  )
+  );
 }
--- front-end/pages/registration.js
+++ front-end/pages/registration.js
@@ -1,107 +1,120 @@
-import Image from 'next/image'
-import styles from '../styles/Registration.module.css'
-import Link from 'next/link';
-import Colors from '../Components/Colors';
-import Button from '../Components/Button';
-import { useState } from 'react';
+import Image from "next/image";
+import styles from "../styles/Registration.module.css";
+import Link from "next/link";
+import Colors from "../Components/Colors";
+import Button from "../Components/Button";
+import { useState } from "react";
 
 //icons
-import {AiFillEye} from 'react-icons/ai'
-import {AiFillEyeInvisible} from 'react-icons/ai'
+import { AiFillEye } from "react-icons/ai";
+import { AiFillEyeInvisible } from "react-icons/ai";
 
-//TODO: 
-    // More form validation  
-    // Password matching hook 
-    // Toggle password visbility icon 
-  
+//TODO:
+// More form validation
+// Password matching hook
+// Toggle password visbility icon
 
 export default function Registration() {
-
-  const [passwordShown, setPasswordShown] = useState(false); 
+  const [passwordShown, setPasswordShown] = useState(false);
 
   const togglePassword = () => {
     setPasswordShown(!passwordShown);
-  }
+  };
   return (
     <div className={styles.body}>
       <Image
         src="/../public/huskies_logo.png"
         layout="fill"
-        objectFit='contain'
+        objectFit="contain"
       />
-    <div className={styles.container}>
-      <div>
+      <div className={styles.container}>
+        <div>
           <h1 className={styles.title}>Register</h1>
-          <hr style={{ color: 'white'}} />
-      </div>
-      <form id="createAccount">
+          <hr style={{ color: "white" }} />
+        </div>
+        <form id="createAccount">
           <div className={styles.group}>
-            <p className={styles.textInputTitle}>
-              Username:
-            </p>
-            <input 
-              type="text" 
-              id="signupUsername" 
-              className={styles.input1} 
-              autoFocus 
+            <p className={styles.textInputTitle}>Username:</p>
+            <input
+              type="text"
+              id="signupUsername"
+              className={styles.input1}
+              autoFocus
               placeholder="Username"
-              required 
+              required
             />
           </div>
           <div className={styles.group}>
-            <p className={styles.textInputTitle}>
-              Email:
-            </p>
-            <input 
-              type="email" 
-              className={styles.input1} 
+            <p className={styles.textInputTitle}>Email:</p>
+            <input
+              type="email"
+              className={styles.input1}
               placeholder="Email Address"
-              required 
+              required
             />
           </div>
           <div className={styles.group}>
             <p className={styles.textInputTitle}>
               Password:
-              {passwordShown ? 
-                <AiFillEye className={styles.inputIcon} size='35px'color={Colors.button} onClick={togglePassword}/> : 
-                <AiFillEyeInvisible className={styles.inputIcon} size='35px' color={Colors.button} onClick={togglePassword}/>
-              } 
+              {passwordShown ? (
+                <AiFillEye
+                  className={styles.inputIcon}
+                  size="35px"
+                  color={Colors.button}
+                  onClick={togglePassword}
+                />
+              ) : (
+                <AiFillEyeInvisible
+                  className={styles.inputIcon}
+                  size="35px"
+                  color={Colors.button}
+                  onClick={togglePassword}
+                />
+              )}
             </p>
-            <input 
+            <input
               type={passwordShown ? "text" : "password"}
-              className={styles.input1} 
-              placeholder="Minimum length 8 characters" 
-              required 
-              minLength={8}  
+              className={styles.input1}
+              placeholder="Minimum length 8 characters"
+              required
+              minLength={8}
             />
           </div>
           <div className={styles.group}>
-              <p className={styles.textInputTitle}>
-                Confirm Password:
-                {passwordShown ? 
-                  <AiFillEye className={styles.inputIcon2} size='35px'color={Colors.button} onClick={togglePassword}/> : 
-                  <AiFillEyeInvisible className={styles.inputIcon2} size='35px' color={Colors.button} onClick={togglePassword}/>
-                } 
-              </p>
-              <input 
-                type={passwordShown ? "text" : "password"}
-                className={styles.input1} 
-                placeholder="Confirm password"
-                minLength={8} 
-              />
+            <p className={styles.textInputTitle}>
+              Confirm Password:
+              {passwordShown ? (
+                <AiFillEye
+                  className={styles.inputIcon2}
+                  size="35px"
+                  color={Colors.button}
+                  onClick={togglePassword}
+                />
+              ) : (
+                <AiFillEyeInvisible
+                  className={styles.inputIcon2}
+                  size="35px"
+                  color={Colors.button}
+                  onClick={togglePassword}
+                />
+              )}
+            </p>
+            <input
+              type={passwordShown ? "text" : "password"}
+              className={styles.input1}
+              placeholder="Confirm password"
+              minLength={8}
+            />
           </div>
-          <button className={styles.button} type="submit">Register</button>
-      </form>
-      <p className={styles.text}>
-        Already have an account? Sign in
-      </p>
-      <Link href="/" passHref >
-        <Button
-          color={Colors.button}
-          text = {'Cancel'}
-        />
-      </Link> 
+          <button className={styles.button} type="submit">
+            Register
+          </button>
+        </form>
+        <p className={styles.text}>Already have an account? Sign in</p>
+        <Link href="/" passHref>
+          <Button color={Colors.button} text={"Cancel"} />
+        </Link>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
