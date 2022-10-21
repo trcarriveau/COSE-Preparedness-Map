5c1feac494724f479b6443b6cc2945de60302975
j-knudson <71792574+j-knudson@users.noreply.github.com>
2022-10-21 08:26:41 -0500

    Updated Colors.js which was lost its export statemetn

---
+++ front-end/Components/grid_components/Semester2.js
@@ -0,0 +1,70 @@
+//This is a component to hold the contents of a single courses and ecs component
+//It will display their output on a particular fall/spring rectangle on the career-map
+
+import { getStepConnectorUtilityClass } from "@mui/material";
+import React from "react";
+import Courses from "./Courses2";
+import Extracurriculars from "../Extracurriculars";
+
+//TODO receive prop data for a semester
+
+const Semester = ({ courses }) => {
+  let classes = [];
+  let ecs = [];
+
+  courses.forEach((course, index) => {
+    if (course.is_extra_curricular == false) {
+      classes.push(course);
+    } else if (course.is_extra_curricular) {
+      ecs.push(course);
+    }
+  });
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        marginLeft: "2px",
+        marginRight: "2px",
+        flexGrow: 1,
+      }}
+    >
+      <div style={styles.coursesContainer}>
+        <Courses courses={classes} />
+      </div>
+      <div style={styles.ecContainer}>
+        <Extracurriculars extracurriculars={ecs} />
+      </div>
+    </div>
+  );
+};
+
+let styles = {
+  coursesContainer: {
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
+
+  ecContainer: {
+    display: "flex",
+    flexDirection: "column-reverse",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+
+  semester: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    // backgroundColor: {color},
+    width: "50%",
+  },
+};
+
+export default Semester;
---
+++ front-end/Components/grid_components/Semesters2.js
@@ -0,0 +1,52 @@
+import { Container, Paper } from "@mui/material";
+import Colors from "../Colors";
+import { useMapDataContext } from "../../contexts/mapdata";
+import { useState } from "react";
+
+import Season from "../Season";
+import Semester from "./Semester2";
+import Course from "../Course";
+import Extracurriculars from "../Extracurriculars";
+import Extracurricular from "../Extracurricular";
+import Courses from "./Courses2";
+
+const Semesters = ({ columns, skill_name }) => {
+  const [mapData, setMapData] = useMapDataContext();
+  return (
+    <>
+      {mapData.years.map((year, index) => (
+        <>
+          {year.semesters.map((semester, index) => (
+            <Paper
+              elevation={2}
+              style={{
+                backgroundColor: `${Colors.PrimaryArr[year.year - 1]}`,
+                display: "flex",
+              }}
+            >
+              {semester.Skills.filter(
+                (skill) => skill.SkillName == skill_name
+              ).map((filteredSkill) => (
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                    marginLeft: "2px",
+                    marginRight: "2px",
+                    flexGrow: 1,
+                  }}
+                >
+                  {/* <Courses courses={filteredSkill.Courses} /> */}
+                  <Semester courses={filteredSkill.Courses} />
+                </div>
+              ))}
+            </Paper>
+          ))}
+        </>
+      ))}
+    </>
+  );
+};
+
+export default Semesters;
