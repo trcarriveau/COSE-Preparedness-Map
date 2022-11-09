603dfe920801fe00c3a303c6162b538133914bfa
tcarriveau <thomas.carriveau@optifacts.com>
2022-11-08 19:23:22 -0600

    Scraping the course id and then using an api call

--- back-end/scripts/web_scraper.js
+++ back-end/scripts/web_scraper.js
@@ -12,35 +12,28 @@ async function scrapeMainPage() {
       console.log(`Caught Error: ${error}`);
     })
 
-  // console.log(`Data`);
-  // console.log(data);
-
   let $ = cheerio.load(data)
 
-
-  $('a.custom-link').each(async (i,e)=>{
-    console.log($(e).attr('href'));
-    let data = await scrapeCoursePage($(e).attr('href'))
-    console.log(`${data.title} === ${data.description}`);
+  $('article.main-content p a.custom-link').each(async (i,e)=>{
+    // console.log($(e).attr('href'));
+    let courseGroupID = $(e).attr('href').split('/').pop()
+    // console.log(`courseGroupID: ${courseGroupID}`);
+    callCourseAPI(courseGroupID)
   })
 }
 
-// scrapeCoursePage('https://catalog.stcloudstate.edu/courses/00161721')
 
 
-
-async function scrapeCoursePage(href) { 
-  let data = await axios.get(`https://catalog.stcloudstate.edu${href}`)
+async function callCourseAPI(courseGroupID) { 
+  let url = `https://app.coursedog.com/api/v1/cm/scsu/courses/search/$filters?courseGroupIds=${courseGroupID}&effectiveDatesRange=2021-08-30%2C2023-01-01&formatDependents=false&includeRelatedData=false&includeCrosslisted=false&includeCourseEquivalencies=false&includePending=false&returnResultsWithTotalCount=false`
+  
+  let data = await axios.get(url)
     .then((response) => {
-      return response.data
+      console.log(`Course Code: ${response.data.data[0].code}`)
+      console.log(`Course Name:${response.data.data[0].name}`)
+      console.log(`Course Description: ${response.data.data[0].description}\n`)
     })
     .catch((error) => {
       console.log(`Caught Error: ${error}`);
     })
-
-    let $ = cheerio.load(data)
-
-    let title = $('#__layout > div > div > div > div > section > div > article > h1').html()
-    let description = $('#aoYks > div > div:nth-child(3) > div > div').html()
-    return { title: title, description: description };
 }
